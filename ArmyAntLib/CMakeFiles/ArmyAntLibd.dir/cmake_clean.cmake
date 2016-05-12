777e167785b2808a4acfe8430d552be65dfdcba1
jason <jason@JASON-DEBIAN.home.jason.net>
2016-05-12 23:09:15 +0800

    * Fixed bugs in Linux

---
+++ ArmyAntLib/CMakeFiles/ArmyAntLibd.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArmyAntLibd.dir/src/ArmyAntLib.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAAes.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AABinary.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAIStream.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAIStream_File.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAIStream_Memory.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AASocket.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/C_AAStream.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/C_AANeuron.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/Java_AANeuron.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAPythonExport.cpp.o"
+  "CMakeFiles/ArmyAntLibd.dir/src/AAJNITools.cpp.o"
+  "libArmyAntLibd.pdb"
+  "libArmyAntLibd.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArmyAntLibd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ArmyAntLib/CMakeFiles/ArmyAntLibd_64.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ArmyAntLibd_64.dir/src/ArmyAntLib.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAAes.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AABinary.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAIStream.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAIStream_File.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAIStream_Memory.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AASocket.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/C_AAStream.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/C_AANeuron.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/Java_AANeuron.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAPythonExport.cpp.o"
+  "CMakeFiles/ArmyAntLibd_64.dir/src/AAJNITools.cpp.o"
+  "libArmyAntLibd_64.pdb"
+  "libArmyAntLibd_64.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArmyAntLibd_64.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
